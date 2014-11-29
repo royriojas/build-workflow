efa2eb3ff94cb68c1404cf5d48e7d5113f5ac057
royriojas <royriojas@gmail.com>
2014-11-29 01:19:48 -0800

    ENH: Replacement for browserify shim to avoid add configuration info to the package.json

---
+++ utils/shimify.js
@@ -0,0 +1,23 @@
+var sFormat = require( 'stringformat' );
+var transformTools = require( 'browserify-transform-tools' );
+var path = require( 'path' );
+
+var options = {
+  excludeExtensions: [ '.json' ],
+  evaluateArguments: true
+};
+
+module.exports = transformTools.makeRequireTransform("requireTransform", options, function(args, opts, cb) {
+  var configData = opts.configData || {};
+  var config = configData.config || {};
+
+  var key = args[0];
+
+  var foundDep = config[key];
+
+  if (foundDep) {
+    return cb(null, sFormat('({0})', foundDep));
+  }
+
+  cb();
+});
