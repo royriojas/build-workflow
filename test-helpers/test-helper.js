2e021b87138784ec19e56e3a1696a50a2aaec9ba
royriojas <royriojas@gmail.com>
2014-11-29 02:43:54 -0800

    REF: Remove need for test-helper.js

--- test-helpers/test-helper.js
+++ test-helpers/test-helper.js
@@ -1,5 +1,8 @@
 (function ( global ) {
+  global.__TEST_ENV__ = true;
+
   require( './bind' );
+  require( 'es6-promise/dist/es6-promise' ).polyfill();
 
   var objUtil = require( './obj-util' );
 
