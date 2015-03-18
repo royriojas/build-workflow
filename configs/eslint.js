cf2b486488690499b9f2c135581bc73e4ca8b8db
royriojas <royriojas@gmail.com>
2015-03-18 02:48:10 -0700

    ENH: Move eslint and eslint formatters to be dependencies of build-workflow

--- configs/eslint.js
+++ configs/eslint.js
@@ -4,7 +4,7 @@ module.exports = function ( grunt /* pkg, options */ ) {
   return {
     options: {
       useCache: !grunt.option( 'skip-cache' ),
-      format: require( 'eslint-friendly-formatter' ),
+      format: require( path.resolve( __dirname, '../node_modules/eslint-friendly-formatter' ) ),
       configFile: path.resolve( __dirname, '../resources/json-configs/eslint.json' )
       //rulePaths: ['conf/rules']
     }
