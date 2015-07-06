3c7c60de8dcceb066cf64bafc6139541f4f870be
royriojas <royriojas@gmail.com>
2015-07-06 03:12:44 -0700

    REF: Factorized bundly into its own module

--- tasks/bundler.js
+++ tasks/bundler.js
@@ -1,26 +1,5 @@
 module.exports = function ( grunt ) {
-  var extend = require( 'extend' );
-  var path = require( 'path' );
   var ES6Promise = require( 'es6-promise' ).Promise;
-  var crypto = require( 'crypto' );
-
-  var md5 = function ( str, encoding ) {
-
-    return crypto
-      .createHash( 'md5' )
-      .update( str + '' )
-      .digest( encoding || 'hex' );
-  };
-
-  var makeMinName = function ( fileName ) {
-    var rgex = /(.+)\.(\w+)$/gi;
-    return fileName.replace( rgex, '$1.min.$2' );
-  };
-
-  var addVersion = function ( fileName, bNumber ) {
-    var rgex = /(.+)\.(\w+)$/gi;
-    return fileName.replace( rgex, '$1.' + bNumber + '.$2' );
-  };
 
   grunt.registerMultiTask( 'bundler', function ( watch ) {
     var me = this;
@@ -40,87 +19,56 @@ module.exports = function ( grunt ) {
     var fileEntries = me.files || [ ];
 
     //console.log('files', fileEntries);
-    var banner = grunt.template.process( opts.banner );
+    opts.banner = grunt.template.process( opts.banner );
 
-    var p = fileEntries.reduce( function ( seq, data ) {
+    var filesPromise = fileEntries.reduce( function ( seq, data ) {
       return seq.then( function () {
+        var bundly = require( '../utils/bundly' ).create( data, opts );
 
-        var src = Array.isArray( data.src ) ? data.src[ 0 ] : data.src;
+        bundly.on( 'files-updated', function ( e, args ) {
+          var files = args.files || [ ];
 
-        var hash = md5( src );
-
-        return new ES6Promise( function ( resolve ) {
-
-          var bundler = require( '../utils/bundler' ).create( 'b-t-' + me.target + '-' + hash );
-
-          var _dest = data.dest;
-          var dest = opts.buildVersion ? addVersion( _dest, opts.buildVersion ) : _dest;
-
-          bundler.on( 'bundler:done', function ( e, args ) {
-
-            grunt.file.write( dest, banner + opts.separator + args.result );
-
-            var duration = Date.now() - args.startTime;
-            logger.ok( 'File written', path.resolve( dest ), 'Time required:', duration / 1000 );
-
-            if ( opts.watch ) {
-              // run forever, waiting for the next bundle cycle
-              var moment = require( 'moment' );
-              logger.subtle( '[' + moment().format( 'MM/DD/YYYY HH:mm:ss' ) + ']', '...Waiting for changes...\n\n' );
-              resolve();
-              return;
-            }
-            if ( !opts.uglify ) {
-              resolve();
-            } else {
-              var tStart = Date.now();
-
-              var uglify = require( 'uglify-js' );
-
-              var result = uglify.minify( args.result, extend( true, {
-                mangle: true
-              }, opts.uglify, { fromString: true } ) );
-
-              var minFile = makeMinName( dest );
-
-              grunt.file.write( minFile, banner + opts.separator + result.code );
-              logger.ok( 'File written', path.resolve( minFile ), 'time required:', (Date.now() - tStart) / 1000 );
-              resolve();
-            }
-
-          } );
-
-          bundler.on( ' bundler:files:updated', function ( e, args ) {
-            var files = args.files || [ ];
-
-            logger.subtle( [
-              '\n updated files \n -',
-              files.join( '\n - ' ),
-              '\n'
-            ].join( ' ' ) );
-          } );
-
-          bundler.on( 'bundler:read-file', function ( e, args ) {
-            logger.log( 'Reading file ', args.file );
-          } );
-
-          bundler.on( 'error', function ( e, err ) {
-            if ( opts.watch ) {
-              logger.important( '\n\nerror:' + JSON.stringify( err.message, null, 2 ) + '\n\n' );
-            } else {
-              logger.error( '\n\nerror:' + JSON.stringify( err.message, null, 2 ) + '\n\n' );
-            }
-          } );
-
-          bundler.bundle( { src: src }, opts );
+          logger.subtle( [
+            '\n updated files \n -',
+            files.join( '\n - ' ),
+            '\n'
+          ].join( ' ' ) );
         } );
+
+        bundly.on( 'read-file', function ( e, args ) {
+          logger.log( 'Reading file ', args.file );
+        } );
+
+        bundly.on( 'file-write', function ( e, args ) {
+          logger.ok( 'File written', args.dest, 'time required:', args.duration );
+        } );
+
+        bundly.on( 'file-write:minify', function ( e, args ) {
+          logger.ok( 'Minified file written', args.dest, 'time required:', args.duration );
+        } );
+
+        bundly.on( 'watch-mode-on', function () {
+          var moment = require( 'moment' );
+          logger.subtle( '[' + moment().format( 'MM/DD/YYYY HH:mm:ss' ) + ']', '...Waiting for changes...\n\n' );
+        } );
+
+        bundly.on( 'error', function ( e, err ) {
+          // error will break the execution
+          // important will just wait until next change to attempt to execute the fix
+          var method = opts.watch ? 'important' : 'error';
+          logger[ method ]( '\n\nerror:' + JSON.stringify( err.message, null, 2 ) + '\n\n' );
+        } );
+
+        return bundly.bundle();
       } );
     }, ES6Promise.resolve() );
 
-    p.then( function () {
+    filesPromise.then( function () {
       if ( !opts.watch ) {
+        // only finish the task if not in watch mode
         done();
       }
     } );
+
   } );
 };
